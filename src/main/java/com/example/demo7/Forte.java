2a5136330d45c8b8d06b6a70d048b34950c1ba1b
mariapivnova <mariapivnova@yandex.ru>
2023-02-09 17:31:17 +0300

    commit

--- src/main/java/com/example/demo7/Forte.java
+++ src/main/java/com/example/demo7/Forte.java
@@ -9,7 +9,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class Forte {
+public class Forte  {
 
     @FXML
     private Button butt1;
